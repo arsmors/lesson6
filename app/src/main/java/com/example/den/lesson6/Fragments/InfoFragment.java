ce44b5c9ea05c36c5eaf8156cd35cbb1fc945727
Arsens <arsens.morins@chi.lv>
2019-01-17 19:26:46 +0200

    2nd

--- app/src/main/java/com/example/den/lesson6/Fragments/InfoFragment.java
+++ app/src/main/java/com/example/den/lesson6/Fragments/InfoFragment.java
@@ -1,13 +1,21 @@
 package com.example.den.lesson6.Fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.example.den.lesson6.Interfaces.PhotoItem;
 import com.example.den.lesson6.R;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -16,13 +24,25 @@ import com.example.den.lesson6.R;
 public class InfoFragment extends Fragment {
 
 
+    public PhotoItem photoItem;
+
+    @BindView(R.id.imageViewImage) ImageView imageViewImage;
+
+
     public InfoFragment() {}
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+//        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.get().load(photoItem.getImgUrl()).into(imageViewImage);
+
+        return view;
     }
 
 }
